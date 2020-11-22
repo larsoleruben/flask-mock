fe6dbe1da83f60f1902c2cb4cda819dd0ae91405
Lars Ole Christoffersen <lars.ole@hey.com>
2020-11-22 23:12:47 +0100

    initial

--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
+from flask.helpers import flash
 from app import app
 from flask import jsonify
 from flask import render_template
+from flask import request
+import os
+
+UPLOAD_FOLDER = '.'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app.secret_key = "84328432u32u5485y34u2?49u$34#823y43289543"
+
 
 @app.route('/')
 def home():
@@ -16,3 +26,36 @@ def ccev():
 @app.route('/template')
 def template():
     return render_template('home.html')
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+       # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return 'No file part'
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return 'No selected file'
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return f'uploaded_file {filename}'
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
